305a7a7144c9f9858041cb21245cb76947c632b4
Grzegorz Pacewicz <g.pacewicz@gmail.com>
2022-09-27 11:38:07 +0200

    Changed js script

--- js/script.js
+++ js/script.js
@@ -1,27 +1,43 @@
-let formElement = document.querySelector(".js-form");
-let amountElement = document.querySelector(".js-form__inputAmount");
-let currencyElement = document.querySelector(".js-form__select");
-let resultElement = document.querySelector(".js-form__result");
+{
+    const formElement = document.querySelector(".js-form");
+    const amountElement = document.querySelector(".js-form__inputAmount");
+    const currencyElement = document.querySelector(".js-form__select");
+    const resultElement = document.querySelector(".js-form__result");
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
+    formElement.addEventListener("submit", (event) => {
+        event.preventDefault();
 
-    let amount = amountElement.value;
-    let currency = currencyElement.value;
-    let rateEur = 4.7011;
-    let rateGbp = 5.4409;
-    let rateUsd = 4.7126;
+        const amount = amountElement.value;
+        const currency = currencyElement.value;
+        const rateEur = 4.7011;
+        const rateGbp = 5.4409;
+        const rateUsd = 4.7126;
 
-    switch (currency) {
-        case "EUR":
-            result = (amount / rateEur).toFixed(2) + `${currency}`;
-            break;
-        case "GBP":
-            result = (amount / rateGbp).toFixed(2) + `${currency}`;
-            break;
-        case "USD":
-            result = (amount / rateUsd).toFixed(2) + `${currency}`;
-            break;
-    }
-    resultElement.value = result;
-});
+
+        switch (currency) {
+            case "EUR":
+                result = (amount / rateEur);
+                break;
+            case "GBP":
+                result = (amount / rateGbp);
+                break;
+            case "USD":
+                result = (amount / rateUsd);
+                break;
+        }
+
+        resultElement.value = result.toFixed(2) + ` ${currency}`;
+    });
+}
+// switch (currency) {
+//     case "EUR":
+//         result = amount / rateEur;
+//         break;
+//     case "GBP":
+//         result = amount / rateGbp;
+//         break;
+//     case "USD":
+//         result = amount / rateUsd;
+//         break;
+// }
+// resultElement.innerHTML = `${amount.toFixed(2) + `${currency} PLN = <strong>${result.toFixed(2)} ${currency} </strong>`;
