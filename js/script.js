8ddcf64e8a2c02963ff6e61036ffaecb480b0cde
Grzegorz Pacewicz <g.pacewicz@gmail.com>
2022-09-29 20:18:50 +0200

    Changed functions in js

--- js/script.js
+++ js/script.js
@@ -25,8 +25,21 @@
       }
     };
 
-    let result = calcuteResult(amount, currency);
+    const onFormSubmit = (event) => {
+      event.preventDefault();
 
-    resultElement.value = `${result.toFixed(2)} ${currency}`;
+      const amount = amountElement.value;
+      const currency = currencyElement.value;
+      let result = calcuteResult(amount, currency);
+
+      resultElement.value = `${result.toFixed(2)} ${currency}`;
+    };
+    formElement.value = addEventListener("submit", onFormSubmit);
   });
+
+  const setResultElement = (result, currency) => {
+    const resultElement = document.querySelector(".js-form__result");
+
+    setResultElement.value = `${result.toFixed(2)} ${currency}`;
+  };
 }
