91d2e15239b33c929ad61599d33689069825e41a
Grzegorz Pacewicz <g.pacewicz@gmail.com>
2022-09-30 10:45:27 +0200

    Changed js code using Wojtek Knott's hints

--- js/script.js
+++ js/script.js
@@ -28,11 +28,13 @@
     const onFormSubmit = (event) => {
       event.preventDefault();
 
-      const amount = amountElement.value;
-      const currency = currencyElement.value;
+      // const amount = amountElement.value;
+      // const currency = currencyElement.value;
+
       let result = calcuteResult(amount, currency);
 
       resultElement.value = `${result.toFixed(2)} ${currency}`;
+      setResultElement(result, currency);
     };
     formElement.value = addEventListener("submit", onFormSubmit);
   });
@@ -40,6 +42,42 @@
   const setResultElement = (result, currency) => {
     const resultElement = document.querySelector(".js-form__result");
 
-    setResultElement.value = `${result.toFixed(2)} ${currency}`;
+    resultElement.value = `${result.toFixed(2)} ${currency}`;
   };
 }
+
+// Previous version
+// {
+//   const formElement = document.querySelector(".js-form");
+//   const amountElement = document.querySelector(".js-form__inputAmount");
+//   const currencyElement = document.querySelector(".js-form__select");
+//   const resultElement = document.querySelector(".js-form__result");
+
+//   formElement.addEventListener("submit", (event) => {
+//     event.preventDefault();
+
+//     const amount = amountElement.value;
+//     const currency = currencyElement.value;
+
+//     const calcuteResult = (amount, currency) => {
+
+//       const rateEur = 4.7011;
+//       const rateGbp = 5.4409;
+//       const rateUsd = 4.7126;
+
+//       switch (currency) {
+//         case "EUR":
+//           return amount / rateEur;
+//         case "GBP":
+//           return amount / rateGbp;
+//         case "USD":
+//           return amount / rateUsd;
+//       }
+//     };
+
+//     let result = calcuteResult(amount, currency);
+
+//     resultElement.value = `${result.toFixed(2)} ${currency}`;
+
+//   });
+// }
